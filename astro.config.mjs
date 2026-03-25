a96db2287d2dbfd1d33197c53fdebd9351e05a64
Daniel Adrian <truedaniyyel@gmail.com>
2026-03-25 08:52:47 -0700

    updated to astro 6.0, with that got rid of old packages like for csp, etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,50 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { resolve } from 'node:path';
-import { SITE } from './src/consts';
+import { defineConfig, fontProviders } from "astro/config";
+import { SITE } from "./src/consts";
 
-import sitemap from '@astrojs/sitemap';
-import { shield } from '@kindspells/astro-shield';
-
-import favicons from 'astro-favicons';
-
-const rootDir = new URL('.', import.meta.url).pathname;
-const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
+import sitemap from "@astrojs/sitemap";
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.CANONICAL_URL,
-  output: 'static',
-  integrations: [sitemap(), shield({
-    sri: { hashesModule: modulePath },
-  }), favicons()],
+  output: "static",
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "DMSans",
+      cssVariable: "--font-dm-sans",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/DMSans.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+
+  integrations: [sitemap(), favicons()],
+
+  security: {
+    csp: {
+      directives: [
+        "default-src 'self'",
+        "object-src 'self'",
+        "connect-src 'self'",
+        "base-uri 'self'",
+        "img-src 'self' https://ik.imagekit.io/truedaniyyel/ data:",
+        "media-src 'self' https://ik.imagekit.io/truedaniyyel/",
+        "font-src 'self' data: https://fonts.gstatic.com",
+        "frame-src 'self' https://www.google.com/",
+        "frame-ancestors 'none'",
+        "worker-src 'self'",
+        "manifest-src 'none'",
+        "form-action 'self'",
+      ],
+    },
+  },
 });
